f7c4a540a04f7df5f609062dc620824031e59b83
Achilleas Tsoumitas <knorcedger@yahoo.gr>
2013-12-10 15:51:50 +0200

    Store now saves the values that it didnt knew when loading them from the browserStorage

--- src/browserStorage.js
+++ src/browserStorage.js
@@ -1,115 +1,104 @@
 /**
- * An angular module that provides easy access to localStorage and sessionStorage
- *
- * @author Achilleas Tsoumitas
- * @version 1.1.0
- * @documentation https://github.com/Knorcedger/angular-browser-storage
- *
- */
+* An angular module that provides easy access to localStorage and sessionStorage
+*
+* @author Achilleas Tsoumitas
+* @version 1.1.2
+* @documentation https://github.com/Knorcedger/angular-browser-storage
+*
+*/
+var browserStorage = angular.module('browserStorage', []);
 
-(function() {
+browserStorage.service('browserStorage', function($window) {
 
-	'use strict';
-
-	var browserStorage = angular.module('browserStorage', []);
-
-	browserStorage.service('browserStorage', function($window) {
-
-		var save = function(type, key, value, expiration) {
-			var object = {
-				data: value
-			};
-			if (expiration) {
-				object.expiration = expiration && Math.round(new Date().getTime() / 1000) + expiration;
-			}
-			$window[type + 'Storage'].setObject(key, object);
+	var save = function(type, key, value, expiration) {
+		var object = {
+			data: value
 		};
+		if (expiration) {
+			object.expiration = expiration && Math.round(new Date().getTime() / 1000) + expiration;
+		}
+		$window[type + 'Storage'].setObject(key, object);
+		
+		return value;
+	};
 
-		var load = function(type, key) {
-			var data = $window[type + 'Storage'].getObject(key) || null;
-			if (data) {
-				// if data was found, check if it has expired, or if no expiration exists
-				if (data.expiration && data.expiration > Math.round(new Date().getTime() / 1000) || !data.expiration) {
-					return data.data;
-				} else {
-					// if it has expired, remove it as well
-					$window[type + 'Storage'].removeItem(key);
-					return null;
-				}
+	var load = function(type, key) {
+		var data = $window[type + 'Storage'].getObject(key) || null;
+		if (data) {
+			// if data was found, check if it has expired, or if no expiration exists
+			if (data.expiration && data.expiration > Math.round(new Date().getTime() / 1000) || !data.expiration) {
+				return data.data;
 			} else {
+				// if it has expired, remove it as well
+				$window[type + 'Storage'].removeItem(key);
 				return null;
 			}
-		};
-
-		var Storage = {
-			local: {
-				save: function(key, value, expiration) {
-					return save('local', key, value, expiration);
-				},
-				load: function(key) {
-					return load('local', key);
-				},
-				remove: function(key) {
-					$window.localStorage.removeItem(key);
-				},
-				clear: function() {
-					$window.localStorage.clear();
-				}
-			},
-			session: {
-				save: function(key, value, expiration) {
-					return save('session', key, value, expiration);
-				},
-				load: function(key) {
-					return load('session', key);
-				},
-				remove: function(key) {
-					$window.sessionStorage.removeItem(key);
-				},
-				clear: function() {
-					$window.sessionStorage.clear();
-				}
-			}
-		};
-
-		return Storage;
-
-	});
-
-	window.Storage.prototype.setObject = function(key, value) {
-		this.setItem(key, JSON.stringify(value));
+		} else {
+			return null;
+		}
 	};
 
-	window.Storage.prototype.getObject = function(key) {
-		var value = this.getItem(key);
-		return value && JSON.parse(value);
+	this.local = {
+		save: function(key, value, expiration) {
+			return save('local', key, value, expiration);
+		},
+		load: function(key) {
+			return load('local', key);
+		},
+		remove: function(key) {
+			$window.localStorage.removeItem(key);
+		},
+		clear: function() {
+			$window.localStorage.clear();
+		}
 	};
-	
-	/**
-	 * A utility service that can be used to save variables either in a temp object (will be lost on refresh) 
-	 * or on browser localStorage.
-	 */
-	browserStorage.service('store', function(browserStorage) {
-		var store = {};
-		return {
-			set: function(key, value, browserStore) {
-				store[key] = value;
-				if (browserStore) {
-					browserStorage.local.save(key, value);
-				}
-			},
-			get: function(key) {
-				if (store.hasOwnProperty(key)) {
-					return store[key];
-				} else {
-					return browserStorage.local.load(key);
-				}
-			},
-			remove: function(key) {
-				delete store[key];
-				browserStorage.local.remove(key);
-			}
-		};
-	});
+	this.session = {
+		save: function(key, value, expiration) {
+			return save('session', key, value, expiration);
+		},
+		load: function(key) {
+			return load('session', key);
+		},
+		remove: function(key) {
+			$window.sessionStorage.removeItem(key);
+		},
+		clear: function() {
+			$window.sessionStorage.clear();
+		}
+	};
+});
 
-}(window));
+window.Storage.prototype.setObject = function(key, value) {
+	this.setItem(key, JSON.stringify(value));
+};
+
+window.Storage.prototype.getObject = function(key) {
+	var value = this.getItem(key);
+	return value && JSON.parse(value);
+};
+
+/**
+ * A utility service that can be used to save variables either in a temp object (will be lost on refresh) 
+ * or on browser localStorage.
+ */
+browserStorage.service('store', function(browserStorage) {
+	var store = {};
+	this.set = function(key, value, browserStore) {
+		store[key] = value;
+		if (browserStore) {
+			browserStorage.local.save(key, value);
+		}
+	};
+	this.get =  function(key) {
+		if (store.hasOwnProperty(key)) {
+			return store[key];
+		} else {
+			store[key] = browserStorage.local.load(key);
+			return store[key];
+		}
+	};
+	this.remove = function(key) {
+		delete store[key];
+		browserStorage.local.remove(key);
+	};
+});
